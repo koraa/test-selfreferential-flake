6d7e0f005c26a289b2d4597ae9edd5657951dfe0
Karolin Varner <karo@cupdev.net>
2025-07-09 13:26:33 +0200

    CLEAR

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, nixpkgs, self-alpha, self-beta }: {
+  outputs = { self, nixpkgs}: {
     packages.x86_64-linux.default =
       let
         pkgs = import nixpkgs { system = "x86_64-linux"; };
       in
       pkgs.writeShellScriptBin "self-referential-test" ''
-        echo "alpha-2 15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
+        echo "15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
       '';
   };
 }
