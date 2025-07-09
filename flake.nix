760b75a17ab6e207645a4679bb62853fc21a3a73
Karolin Varner <karo@cupdev.net>
2025-07-09 13:27:26 +0200

    beta-3

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
         pkgs = import nixpkgs { system = "x86_64-linux"; };
       in
       pkgs.writeShellScriptBin "self-referential-test" ''
-        echo "alpha-3 15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
+        echo "beta-3 15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
       '';
   };
 }
