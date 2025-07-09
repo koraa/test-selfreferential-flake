f8b47e037dbbe6706dd12c1c350e85096895aa16
Karolin Varner <karo@cupdev.net>
2025-07-09 10:39:36 +0200

    Test self-ref flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
   description = "Minimal self-referential flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.self-main-branch.url = "github:koraa/test-selfreferential-flake";
+
+  outputs = { self, nixpkgs, self-main-branch }: {
+    packages.x86_64-linux.main-branch = self-main-branch.packages.x86_64-linux.default;
     packages.x86_64-linux.default =
       let
         pkgs = import nixpkgs { system = "x86_64-linux"; };
       in
       pkgs.writeShellScriptBin "self-referential-test" ''
-        echo "15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
+        echo "NEW 15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
       '';
   };
 }
