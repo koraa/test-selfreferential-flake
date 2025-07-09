62f2e41fb634a32a92d77b8882964b40160a6a32
Karolin Varner <karo@cupdev.net>
2025-07-09 18:52:13 +0200

    gamma-3

--- flake.nix
+++ flake.nix
@@ -2,14 +2,19 @@
   description = "Minimal self-referential flake";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.self-alpha-3.url = "github:koraa/test-selfreferential-flake?ref=d02a29ed0b2771d6d96e071888f1194f6887d038";
+  inputs.self-beta-3.url = "github:koraa/test-selfreferential-flake?ref=760b75a17ab6e207645a4679bb62853fc21a3a73";
 
-  outputs = { self, nixpkgs}: {
-    packages.x86_64-linux.default =
+  outputs = { self, nixpkgs, self-alpha-3, self-beta-3 }: rec {
+    packages.x86_64-linux.alpha-3 = self-alpha-3.packages.x86_64-linux.default;
+    packages.x86_64-linux.beta-3 = self-beta-3.packages.x86_64-linux.default;
+    packages.x86_64-linux.gamma-3 =
       let
         pkgs = import nixpkgs { system = "x86_64-linux"; };
       in
       pkgs.writeShellScriptBin "self-referential-test" ''
-        echo "beta-3 15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
+        echo "gamma-3 15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
       '';
+    packages.x86_64-linux.default = packages.x86_64-linux.gamma-3;
   };
 }
