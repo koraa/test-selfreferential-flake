4c1fae3235918bcecce07690cff6b29a9efa4237
Karolin Varner <karo@cupdev.net>
2025-07-09 13:25:45 +0200

    alpha-2

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
         pkgs = import nixpkgs { system = "x86_64-linux"; };
       in
       pkgs.writeShellScriptBin "self-referential-test" ''
-        echo "15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
+        echo "alpha-2 15f8830a-9f14-429d-b1c5-283bf4f2c9ee"
       '';
   };
 }
